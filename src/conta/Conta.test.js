bb3c6231a5ec6dbcc98c3a922ba57d2098944a37
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-01-16 06:42:39 -0300

    Simulando uma função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Conta from './Conta';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('Componente de conta', () => {
     it('Exibir o saldo da conta com formatação monetária', () => {
@@ -8,5 +8,14 @@ describe('Componente de conta', () => {
         const saldo = screen.getByTestId('saldo-conta');
 
         expect(saldo.textContent).toBe('R$ 1000')
+    })
+
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />)
+
+        fireEvent.click(screen.getByText('Realizar operação'))
+        expect(funcaoRealizarTransacao).toHaveBeenCalled()
     })
 })
