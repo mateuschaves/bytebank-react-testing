c3a3d1c632dff16f8f8d7c6a2376bb5a364f30c3
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-01-16 05:59:28 -0300

    React Testing Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App, { calcularNovoSaldo } from './app';
 
 describe('Componente principal', () => {
@@ -28,6 +28,23 @@ describe('Componente principal', () => {
             }
             const novoSaldo = calcularNovoSaldo(valores, 150)
             expect(novoSaldo).toBe(100)
+        })
+
+        it('que é um saque, a transação deve ser realizada', () => {
+            const { getByText, getByTestId, getByLabelText } = render(<App />);
+
+            const saldo = getByText('R$ 1000');
+            const transacao = getByLabelText('Saque');
+            const valor = getByTestId('valor');
+            const botaoTransacao = getByText('Realizar operação');
+
+            expect(saldo.textContent).toBe('R$ 1000');
+
+            fireEvent.change(valor, { target: { value: 10 } })
+            fireEvent.click(transacao, { target: { value: valor } });
+            fireEvent.click(botaoTransacao);
+
+            expect(saldo.textContent).toBe('R$ 990');
         })
     })
 })
